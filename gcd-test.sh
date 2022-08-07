62e5c11e27307d1282c89d4a50b8bd506e1056f6
z22064hs <z22064hs@aiit.ac.jp>
2022-08-07 11:26:15 +0900

    提出準備完了

--- gcd-test.sh
+++ gcd-test.sh
@@ -18,7 +18,7 @@ ERROR_EXIT(){
 #[TEST 101]正常系
 testnum='101'; check='引数・自然数正常($1>$2)'
 ./gcd.sh 11 35 > $result || ERROR_EXIT "[ERR  $testnum] NG $check >> 異常終了"
-if [ `grep -c CGD=* $result` == 1 ]; then
+if [ `grep -c GCD=* $result` == 1 ]; then
     echo "[TEST $testnum] OK $check >> 正常終了"; else
     ERROR_EXIT "[ERR  $testnum] NG $check >> 出力異常"
 fi
@@ -26,7 +26,7 @@ fi
 #[TEST 102]正常系
 testnum='102'; check='引数・自然数正常($2>$1)'
 ./gcd.sh 35 11 > $result || ERROR_EXIT "[ERR  $testnum] NG $check >> 異常終了"
-if [ `grep -c CGD=* $result` == 1 ]; then
+if [ `grep -c GCD=* $result` == 1 ]; then
     echo "[TEST $testnum] OK $check >> 正常終了"; else
     ERROR_EXIT "[ERR  $testnum] NG $check >> 出力異常"
 fi
@@ -34,7 +34,7 @@ fi
 #[TEST 103]正常系
 testnum='103'; check='引数・自然数正常($1=$2)'
 ./gcd.sh 11 11 > $result || ERROR_EXIT "[ERR  $testnum] NG $check >> 異常終了"
-if [ `grep -c CGD=* $result` == 1 ]; then
+if [ `grep -c GCD=* $result` == 1 ]; then
     echo "[TEST $testnum] OK $check >> 正常終了"; else
     ERROR_EXIT "[ERR  $testnum] NG $check >> 出力異常"
 fi
@@ -54,23 +54,58 @@ diff $result $ans >> $err || ERROR_EXIT "[ERR  $testnum] NG $check >> エラー�
 echo "[TEST $testnum] OK $check >> エラー出力終了"
 
 #[TEST 203]異常系
-testnum='203'; check='引数数値以外'
+testnum='203'; check='引数３つ'
+./gcd.sh 10 20 30 2> $result && ERROR_EXIT "[ERR  $testnum] NG $check >> 正常終了"
+echo '[ERROR 01] 引数は自然数を２つ入力してください。' > $ans
+diff $result $ans >> $err || ERROR_EXIT "[ERR  $testnum] NG $check >> エラーメッセージ相違"
+echo "[TEST $testnum] OK $check >> エラー出力終了"
+
+#[TEST 211]異常系
+testnum='211'; check='引数数値以外'
 ./gcd.sh a a 2> $result && ERROR_EXIT "[ERR  $testnum] NG $check >> 正常終了"
 echo '[ERROR 02] 引数は自然数を２つ入力してください。' > $ans
 diff $result $ans >> $err || ERROR_EXIT "[ERR  $testnum] NG $check >> エラーメッセージ相違"
 echo "[TEST $testnum] OK $check >> エラー出力終了"
 
-#[TEST 204]異常系
-testnum='204'; check='引数１に自然数以外'
+#[TEST 221]異常系
+testnum='221'; check='引数１にゼロ値'
 ./gcd.sh 0 10 2> $result && ERROR_EXIT "[ERR  $testnum] NG $check >> 正常終了"
 echo '[ERROR 03] 引数は自然数を２つ入力してください。' > $ans
 diff $result $ans >> $err || ERROR_EXIT "[ERR  $testnum] NG $check >> エラーメッセージ相違"
 echo "[TEST $testnum] OK $check >> エラー出力終了"
 
-#[TEST 205]異常系
-testnum='205'; check='引数２に自然数以外'
+#[TEST 222]異常系
+testnum='222'; check='引数２にゼロ値'
 ./gcd.sh 10 0 2> $result && ERROR_EXIT "[ERR  $testnum] NG $check >> 正常終了"
 echo '[ERROR 04] 引数は自然数を２つ入力してください。' > $ans
+diff $result $ans >> $err || ERROR_EXIT "[ERR  $testnum] NG $check >> エラーメッセージ相違"
+echo "[TEST $testnum] OK $check >> エラー出力終了"
+
+#[TEST 223]異常系
+testnum='223'; check='引数１に負数'
+./gcd.sh -1 10 2> $result && ERROR_EXIT "[ERR  $testnum] NG $check >> 正常終了"
+echo '[ERROR 03] 引数は自然数を２つ入力してください。' > $ans
+diff $result $ans >> $err || ERROR_EXIT "[ERR  $testnum] NG $check >> エラーメッセージ相違"
+echo "[TEST $testnum] OK $check >> エラー出力終了"
+
+#[TEST 224]異常系
+testnum='224'; check='引数２に負数'
+./gcd.sh 10 -1 2> $result && ERROR_EXIT "[ERR  $testnum] NG $check >> 正常終了"
+echo '[ERROR 04] 引数は自然数を２つ入力してください。' > $ans
+diff $result $ans >> $err || ERROR_EXIT "[ERR  $testnum] NG $check >> エラーメッセージ相違"
+echo "[TEST $testnum] OK $check >> エラー出力終了"
+
+#[TEST 225]異常系
+testnum='225'; check='引数１に小数'
+./gcd.sh 1.2 10 2> $result && ERROR_EXIT "[ERR  $testnum] NG $check >> 正常終了"
+echo '[ERROR 02] 引数は自然数を２つ入力してください。' > $ans
+diff $result $ans >> $err || ERROR_EXIT "[ERR  $testnum] NG $check >> エラーメッセージ相違"
+echo "[TEST $testnum] OK $check >> エラー出力終了"
+
+#[TEST 226]異常系
+testnum='226'; check='引数２に小数'
+./gcd.sh 10 1.2 2> $result && ERROR_EXIT "[ERR  $testnum] NG $check >> 正常終了"
+echo '[ERROR 02] 引数は自然数を２つ入力してください。' > $ans
 diff $result $ans >> $err || ERROR_EXIT "[ERR  $testnum] NG $check >> エラーメッセージ相違"
 echo "[TEST $testnum] OK $check >> エラー出力終了"
 
